ac3cfc40bb013079c77db9501e9a2e584236cbb2
Slmogrovejo <slmogrovejo@espe.edu.ec>
2023-08-15 20:23:13 -0500

    Main

---
+++ BlazorWebAssemblyJuego/Models/SquareModel.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorWebAssemblyJuego.Models
+{
+    public class SquareModel
+    {
+        private bool isShown;
+        public int Id { get; set; }
+        public string Style { get; set; }
+
+        public char RandomLetter { get; set; }
+
+        public bool IsShown
+        {
+            get => isShown;
+            set
+            {
+                isShown = value;
+                if (isShown)
+                {
+                    Style = "mole";
+                }
+                else
+                {
+                    Style = "";
+                }
+            }
+        }
+    }
+}
---
+++ BlazorWebAssemblyJuego/Pages/BlazorJuegoComponent.cs
@@ -0,0 +1,29 @@
+﻿using BlazorWebAssemblyJuego.Models;
+
+namespace BlazorWebAssemblyJuego.Pages
+{
+    public partial class BlazorJuegoComponent
+    {
+        int score = 0;
+        int currentTime = 60;
+        int hitPosition = 0;
+        int hitPosition1 = 100;
+        int hitPosition2 = 100;
+        string message = "";
+        int gameSpeed = 500;
+        bool isGameRunning = true;
+
+        int[] Letras = new int[7];
+
+        public List<SquareModel> Squares { get; set; } =
+            new List<SquareModel>();
+
+        public BlazorJuegoComponent() {
+            for(int i = 0; i <49 ; i++)
+            {
+                Squares.Add(new SquareModel() { Id  = i });
+            }
+        
+        }
+    }
+}
